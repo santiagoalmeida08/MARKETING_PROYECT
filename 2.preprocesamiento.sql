c552ad1c794ba569f04c51ed205281110b7e3900
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-03-25 17:10:33 -0500

    inicio_preprocesamiento_V2

--- 2.preprocesamiento.sql
+++ 2.preprocesamiento.sql
@@ -1,16 +1,19 @@
 --PREPROSESAMIENTO
 
 -- BASE RATINGS
+
 --1. Cambiar formato de timestamp a date
 
 DROP TABLE IF EXISTS ratings_alter;
-CREATE TABLE ratings_alter AS 
-SELECT * FROM ratings;
-ALTER TABLE ratings_alter ADD  fecha DATE;
-UPDATE rating_alter SET fecha = date(timestamp, 'unixepoch');
-ALTER TABLE ratings_alter DROP timestamp;
+CREATE TABLE ratings_alter AS SELECT * FROM ratings;
+ALTER TABLE ratings_alter ADD fecha DATETIME;
+UPDATE ratings_alter SET fecha = DATETIME(ratings_alter.timestamp, 'unixepoch');
 
+--2. SEPARAR EL MES DE LA FECHA  
+ALTER TABLE ratings_alter ADD mes INT;
+UPDATE ratings_alter SET mes = strftime('%m', ratings_alter.fecha);
 
-
-
+--3-BORRAR COLUMNAS INNECESARIAS TIMESTAMP Y FECHA
+ALTER TABLE ratings_alter DROP COLUMN timestamp;
+ALTER TABLE ratings_alter DROP COLUMN fecha;
 
--- 3.exploracion.py
+++ 3.exploracion.py
@@ -5,17 +5,18 @@ import funciones as fn
 
 # Conectarse a la base de datos 
 conn = sql.connect('data_marketing//db_movies') # identifica bases de datos
-cur = conn.cursor() # permite ejecutar comandos SQL
+cur = conn.cursor() # permite e]jecutar comandos SQL
+
 
 #Verificación de conexión
 cur.execute('SELECT name FROM sqlite_master WHERE type="table"') # selecciona las tablas de la base de datos
 cur.fetchall() # observamos las tablas que tiene la base de datos
 
-# Ejecutar consultas con pandas #
+# Ejecutar consultas con pandas para observar el contenido de las tablas #
 
 ratings = pd.read_sql('SELECT * FROM ratings', conn) # contiene las calificaciones que los usuarios dieron a las películas
 movie = pd.read_sql('SELECT * FROM movies', conn)# contiene información de las películas
-#ratings2 = pd.read_sql('SELECT * FROM ratings2', conn) 
+
 
 # Observar contenido de las tablas y hacer consideraciones para preprocesamiento 
 
@@ -29,21 +30,13 @@ movie.sample(5) # separar el año de la película del nombre , poner nombre de v
 
 # Convertir el timestamp a formato fecha en la tabla ratings
 
-p = ratings.copy()
-p['fecha'] = pd.to_datetime(p['timestamp'], unit='s') # convertir timestamp a fecha 
-
 fn.ejecutar_sql('2.preprocesamiento.sql',conn) # ejecutar script de preprocesamiento
+ratings_fecha = pd.read_sql('SELECT * FROM ratings_alter', conn)
+ratings_fecha.info()
 
-ratings_alter.info()
+# 1. Exploración de datos tabla ratings_alter
 
-
-
-
-# 1. Explorar la tabla ratings
-
-## CONVERTIR EL TIMESTAMP A FORMATO FECHA DE LA TABLA RATINGS Y DESPUES REALIZAR FILTROS DE DATOS##
-
-#¿Cuantas películas ha visto cada usuario?#POR MES  --- SE NECESITA CONVERSION DE TIMESTAMP EN PREPROCESAMIENTO
+#¿Cuantas películas ha visto cada usuario?
 
 s = pd.read_sql(""" SELECT userId as usuario, count(*) as numero_peliculas
                     FROM ratings
@@ -62,6 +55,12 @@ s2 = pd.read_sql(""" SELECT userId, count(*) num_peliculas
                 ORDER BY num_peliculas""",conn)
 
 sns.histplot(s2['num_peliculas'], color='orange', bins=70 )
+
+#¿Cuantas peliculas ve el usuario mensualmente?
+
+# ---
+
+
 
 #¿Cuales son las calificaciones mas frecuentes que los usuarios dan a las películas?
 
@@ -82,7 +81,6 @@ mc = pd.read_sql(""" SELECT movieId as pelicula, rating as calificacion,
                      GROUP BY calificacion
                      ORDER BY conteo ASC""",conn)
 
-sns.histplot(mc[''], color='orange', bins=70)   
 
 # Por cuantos usuarios ha sido calificada cada película?
 
