23a3e2716dcf76d55ee152dbc694910a767b739c
sara20arenash <sara.arenash@udea.edu.co>
2024-04-03 15:52:24 -0500

    modelo de filtro colaborativo

--- 4.modelos.py
+++ 4.modelos.py
@@ -305,6 +305,116 @@ print(interact(recomendar))
 
 
 
+############################################################################
+#####4. Sistema de recomendación filtro colaborativo #####
+############################################################################
+
+### datos originales en pandas
+## knn solo sirve para calificaciones explicitas
+ratings=pd.read_sql('select * from rating_final', conn)
+
+
+####los datos deben ser leidos en un formato espacial para surprise
+reader = Reader(rating_scale=(0, 5)) ### la escala de la calificación
+###las columnas deben estar en orden estándar: user item rating
+data   = Dataset.load_from_df(ratings[['user_id','movie_id','rating']], reader)
+
+
+#####Existen varios modelos 
+models=[KNNBasic(),KNNWithMeans(),KNNWithZScore(),KNNBaseline()] 
+results = {}
+
+###knnBasiscs: calcula el rating ponderando por distancia con usuario/Items
+###KnnWith means: en la ponderación se resta la media del rating, y al final se suma la media general
+####KnnwithZscores: estandariza el rating restando media y dividiendo por desviación 
+####Knnbaseline: calculan el desvío de cada calificación con respecto al promedio y con base en esos calculan la ponderación
+
+
+#### for para probar varios modelos ##########
+model=models[1]
+for model in models:
+ 
+    CV_scores = cross_validate(model, data, measures=["MAE","RMSE"], cv=5, n_jobs=-1) # se usan medidas de regresion ya que se esta prediciendo una variable numerica
+    
+    result = pd.DataFrame.from_dict(CV_scores).mean(axis=0).\
+             rename({'test_mae':'MAE', 'test_rmse': 'RMSE'})
+    results[str(model).split("algorithms.")[1].split("object ")[0]] = result
+
+
+performance_df = pd.DataFrame.from_dict(results).T
+performance_df.sort_values(by='RMSE')
+
+
+#el modelo de knns.KNNWithMeans el RMSE no varia mucho comparado con los demas modelos y ademas el tiempo es el menor por esto se selecciona este modelo
+###################se escoge el mejor knn withmeans#########################
+param_grid = { 'sim_options' : {'name': ['msd','cosine'], \
+                                'min_support': [10,5], \
+                                'user_based': [False, True]}
+             }# si no hay minimo 5 personas que lo calificaron no recomiende el producto 
+
+## min support es la cantidad de items o usuarios que necesita para calcular recomendación
+## name medidas de distancia
+
+### se afina si es basado en usuario o basado en ítem
+
+gridsearchKNNWithMeans = GridSearchCV(KNNWithMeans, param_grid, measures=['rmse'], \
+                                      cv=2, n_jobs=1)# el cv y el n_jobs puede variar
+                                    
+gridsearchKNNWithMeans.fit(data)
+
+
+gridsearchKNNWithMeans.best_params["rmse"]
+gridsearchKNNWithMeans.best_score["rmse"]
+gs_model=gridsearchKNNWithMeans.best_estimator['rmse'] ### mejor estimador de gridsearch
+
+
+################# Entrenar con todos los datos y Realizar predicciones con el modelo afinado
+
+trainset = data.build_full_trainset() ### esta función convierte todos los datos en entrnamiento, las funciones anteriores dividen  en entrenamiento y evaluación
+model=gs_model.fit(trainset) ## se reentrena sobre todos los datos posibles (sin dividir)
+
+
+
+predset = trainset.build_anti_testset() ### crea una tabla con todos los usuarios y las peliculas que no han visto
+#### en la columna de rating pone el promedio de todos los rating, en caso de que no pueda calcularlo para un item-usuario
+len(predset)
+
+predictions = gs_model.test(predset) ### función muy pesada, hace las predicciones de rating para todos las peliculas que no hay leido un usuario
+### la funcion test recibe un test set constriuido con build_test method, o el que genera crosvalidate
+
+####### la predicción se puede hacer para un libro puntual
+#model.predict(uid=269397, iid='0446353205',r_ui='') ### uid debía estar en número e isb en comillas
+
+predictions_df = pd.DataFrame(predictions) ### esta tabla se puede llevar a una base donde estarán todas las predicciones
+predictions_df.shape
+predictions_df.head()
+predictions_df['r_ui'].unique() ### promedio de ratings
+predictions_df.sort_values(by='est',ascending=False)
+
+
+##### funcion para recomendar los 10 peliculas con mejores predicciones y llevar base de datos para consultar resto de información
+user = pd.read_sql('select distinct (user_id) as user_id from rating_final',conn)
+user
+def recomendaciones(user_id=list(user['user_id'].value_counts().index),n_recomend=10):
+    
+    predictions_userID = predictions_df[predictions_df['uid'] == user_id].\
+                    sort_values(by="est", ascending = False).head(n_recomend)
+
+    recomendados = predictions_userID[['iid','est']]
+    recomendados.to_sql('reco',conn,if_exists="replace")
+    
+    recomendados=pd.read_sql('''select a.*, b.pelicula 
+                             from reco a left join movie_final2 b
+                             on a.iid=b.movieId ''', conn)
+
+    return(recomendados)
+
+
+ 
+recomendaciones(user_id=609,n_recomend=10)
+
+
+print(interact(recomendaciones))
 
 
 
